84b0426a4f43c9dda1ee9e662df7cc1f575e0115
AlbinMichael <134335972+AlbinMichael@users.noreply.github.com>
2023-08-01 19:33:47 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>NAVIGATION BAR</title>
+</head>
+<body>
+<nav class="navbar has-background-primary">
+  <div class="container">
+    <div class="navbar-brand">
+      <label class="navbar-item logo has-text-white">MOGA PIZZA</label>
+    </div>
+    <div class="navbar-menu">
+      <ul class="navbar-end">
+        <li class="navbar-item"><a class="navbar-link has-text-white" href="view.php">View table</a></li>
+        <li class="navbar-item"><a class="navbar-link has-text-white" href="index.php">Main page</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+</body>
+    </html>
